ae506719e6340ed4042d35b3bbb58cc7c6b9e5f7
Alexander <info@alexandersstudio.uk>
2022-09-08 09:00:05 +0100

    API start

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const formulario = document.querySelector('.weather__search');
 const city = document.querySelector('.weather__search--input');
+const leftSide = document.querySelector('.weather__temperature');
+let newCountry;
+let newCity;
 
 start();
 
@@ -14,25 +17,177 @@ function citySearch(e) {
   if (city.value === '') {
     printMessage('Error, the field search is required.');
   } else {
-    readApi();
+    newCity = city.value;
+    weatherApi();
+    formulario.reset();
   }
 }
 
 //function read API response
 
-function readApi() {
+function weatherApi() {
   const keyID = 'bf5d406b7fbe90761dc05f9369e465d0';
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${keyID}`;
 
   fetch(URL)
     .then((response) => response.json())
-    .then((data) => dataAPI(data));
+    .then((data) => weatherDataAPI(data));
 }
 
-// Data fromAPI
+// Data Waether fromAPI
 
-function dataAPI(content) {
-  console.log(content);
+function weatherDataAPI(content) {
+  cleanHtml();
+
+  // general Data
+  const { name, cod, timezone, dt } = content;
+
+  //Data from MAIN information
+  const { temp } = content.main;
+
+  const celcius = parseInt(temp - 273.15);
+
+  //DAta to get country and sunset
+  const { country, sunrise, sunset } = content.sys;
+
+  const myDate = new Date();
+
+  const optionDay = {
+    weekday: 'long',
+    day: 'numeric',
+  };
+  const optionYear = { year: 'numeric' };
+  const finalDay = new Intl.DateTimeFormat('en-UK', optionDay).format(myDate);
+  const finalYear = new Intl.DateTimeFormat('en-UK', optionYear).format(myDate);
+
+  newCountry = country;
+  //   newsApi();
+
+  //Data to get the winds details
+  const { speed } = content.wind;
+
+  //Data to get main weather condition icon
+  const { main, description } = content.weather[0];
+
+  //Layout and structure fields
+
+  const location = document.createElement('div');
+
+  location.innerHTML = `
+
+       <div class="weather__temperature__location">
+           <img src="img/pin.svg" alt="location" />
+           <p>${newCity}, ${country} </p>
+       </div>      
+      
+      `;
+
+  const result = document.createElement('div');
+
+  // Getting the date of the
+
+  // const days = moment().format('dddd');
+  // console.log(days);
+
+  result.innerHTML = `
+        
+         <div class="weather__temperature__result">
+              <img src="img/cloudly-icon.svg" alt="" />
+              <h1 class="h1">${celcius} <sup>&#8728;c</sup></h1>
+              <p>
+               ${finalDay},
+
+                <span> ${finalYear} </span>
+              </p>
+        </div>
+
+  `;
+
+  //   Appenchild for data
+
+  leftSide.appendChild(location);
+  leftSide.appendChild(result);
+}
+
+//function read API from news
+
+function newsApi() {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'fa8acf2c65mshb42938ea644383bp1216f8jsn5311ae779480',
+      'X-RapidAPI-Host': 'google-news1.p.rapidapi.com',
+    },
+  };
+
+  fetch(
+    `https://google-news1.p.rapidapi.com/search?q=${city.value}&country=${newCountry}&lang=en-US&when=30d&limit=3`,
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => printNews(data));
+}
+
+//Funcion to print the news from API
+
+function printNews(data) {
+  console.log(data);
+
+  const articles = data.articles;
+  const cardContainer = document.querySelector('.slide-container');
+
+  //   articles.forEach((item) => {
+  //     const { title } = item;
+  //     const card = document.createElement('div');
+
+  //     card.innerHTML = `
+  //       <div class="swiper-slide card">
+  //             <div class="card__top">
+  //                       <div class="card__top__resource">
+  //                         <p>BBC news</p>
+  //                       </div>
+
+  //                       <div class="card__top__date">
+  //                         <img src="img/calendar.svg" alt="cdate-news" />
+  //                         <span>25/08/2022</span>
+  //                       </div>
+  //              </div>
+
+  //              <div class="card__content">
+  //              <div class="card__content--text">
+  //                <h5 class="h5">
+  //                 ${title}
+  //                </h5>
+
+  //                <p>
+  //                  As The Tokyo Olympics Get Underway And Rich Countries
+  //                  Drop Restrictions Lorem ipsum dolor sit amet.
+  //                </p>
+  //              </div>
+
+  //              <div class="card__content--img">
+  //                <img src="img/img.svg" alt="news-image" />
+  //              </div>
+  //            </div>
+
+  //              <div class="card__link">
+  //                       <a href="https://www.google.co.uk" target="_blank"
+  //                         >read more</a
+  //                       >
+  //              </div>
+
+  //         </div>
+  //  `;
+  //     cardContainer.appendChild(card);
+  //   });
+}
+
+//Clean HTML function for weather the
+
+function cleanHtml() {
+  while (leftSide.firstChild) {
+    leftSide.removeChild(leftSide.firstChild);
+  }
 }
 
 //Erro fucntion message
